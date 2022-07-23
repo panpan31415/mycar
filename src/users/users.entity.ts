c6a27d0cee02789b7ed5be7fd7dd9f56a6f1b352
panpan31415 <panpan31415@gmail.com>
2022-07-23 21:31:42 +0200

    lecture 51 finished, entity hooks

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,11 +1,34 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  AfterInsert,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  AfterRemove,
+  AfterUpdate,
+} from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
-  @Column()
+
   email: string;
   @Column()
   password: string;
+
+  //hook methods
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserted User with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('removed user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('updated user with id', this.id);
+  }
 }
