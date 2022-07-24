acd544305ce369786ce65fd0c51d509e7e969a43
panpan31415 <panpan31415@gmail.com>
2022-07-24 20:04:57 +0200

    lecture 59 finished

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
@@ -9,5 +20,44 @@ export class UsersController {
   createUser(@Body() body: CreateUserDto) {
     const { email, password } = body;
     this.userService.create(email, password);
+  }
+
+  @Get('/:id')
+  // anything saved in request is string, so id has a type of string
+  async deleteUser(@Param('id') id: string) {
+    try {
+      return await this.userService.remove(parseInt(id));
+    } catch (err) {
+      throw new NotFoundException(`user with id ${id} is not found`);
+    }
+  }
+
+  @Delete('/:id')
+  // anything saved in request is string, so id has a type of string
+  async findUser(@Param('id') id: string) {
+    try {
+      return await this.userService.remove(parseInt(id));
+    } catch (error) {
+      throw new NotFoundException(`user with id ${id} is not found`);
+    }
+  }
+
+  @Get()
+  async findByEmail(@Query('email') email: string) {
+    try {
+      const user = await this.userService.find(email);
+      return user;
+    } catch (err) {
+      throw new Error(`user with email ${email} is not found`);
+    }
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto) {
+    try {
+      return await this.userService.update(parseInt(id), body);
+    } catch (err) {
+      throw new NotFoundException(`user with id ${id} is not found`);
+    }
   }
 }
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -12,9 +12,7 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
   email: string;
-  
   @Column()
   password: string;
 
